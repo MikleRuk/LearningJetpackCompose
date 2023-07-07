40b79d95d5c805e79dda95f3601af8eafa94f4d0
MikleRuk <MikleRuk>
2023-07-07 16:06:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.learningjetpackcompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+
+                CircleItem()
+
+            }
+        }
+
+    }
+}
+
+
+@Composable
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+
+                when(++counter.value){
+                    10 -> color.value = Color.Red
+                    15 -> color.value = Color.Black
+                }
+
+                       },
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 20.sp)
+        )
+
+
+    }
+
+
+}
+
